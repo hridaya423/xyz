737fb54785ea89db804288e2fc925e7618d14fd0
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-02-20 12:53:52 -0800

    fix crosshair

--- src/app/components/HUD.tsx
+++ src/app/components/HUD.tsx
@@ -36,9 +36,9 @@ export default function HUD({ ammo, reloading, health, onUnpause }: HUDProps) {
       <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none">
         <div className="relative">
           {/* Vertical line */}
-          <div className="absolute h-4 w-[2px] bg-white -translate-y-1/2 left-1/2 -translate-x-1/2 -top-[2px]"></div>
+          <div className="absolute h-4 w-[2px] bg-white -translate-y-1/2 left-1/2 -translate-x-1/2 -top+[2px]"></div>
           {/* Horizontal line */}
-          <div className="absolute w-4 h-[2px] bg-white -translate-x-1/2 top-1/2 -translate-y-1/2 -left-[2px]"></div>
+          <div className="absolute w-4 h-[2px] bg-white -translate-x-1/2 top-1/2 -translate-y-1/2 -left+[2px]"></div>
           {/* Center dot */}
           <div className="absolute w-1 h-1 bg-white rounded-full -translate-x-1/2 -translate-y-1/2"></div>
         </div>
